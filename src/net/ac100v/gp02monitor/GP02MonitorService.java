a01f253877d482531c82e568fd286a9f091f07e6
snickerjp <snicker_jp@hotmail.com>
2014-05-14 14:47:34 +0900

    new model GL06P GL10P

--- src/net/ac100v/gp02monitor/GP02MonitorService.java
+++ src/net/ac100v/gp02monitor/GP02MonitorService.java
@@ -198,7 +198,11 @@ public class GP02MonitorService extends Service {
     	String routerModel = preferences.getString("key_router_model", "GP02");
     	if (routerModel.equals("GL04P")) {
     		driver = new GL04PDriver();
-    	} else {
+    	} else if (routerModel.equals("GL06P")) {
+    		driver = new GL06PDriver();
+		} else if (routerModel.equals("GL10P")) {
+    		driver = new GL10PDriver();
+		} else {
     		driver = new GP02Driver();
     	}
     	
